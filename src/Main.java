6b9b986514c14f9c3f84740edbdbf683e02050ad
malione17 <turan_memoli_2000@hotmail.com>
2024-12-10 00:42:47 +0300

    mali1

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        float boy,kilo,index;
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz: ");
-        boy = inp.nextFloat();
-
-
-        System.out.print("Lütfen kilonuzu giriniz: ");
-        kilo = inp.nextFloat();
-
-        index = kilo / (boy * boy);
-        System.out.printf("Vücut kitle indexiniz: %.2f\n",index);
-
+       float armut,elma,domates,muz,patlican,toplam;
+       Scanner input = new Scanner(System.in);
+        System.out.print("armut kaç kilo ? : ");
+        armut = input.nextFloat();
+        System.out.print("elma kaç kilo ? : ");
+        elma = input.nextFloat();
+        System.out.print("domates kaç kilo ? : ");
+        domates = input.nextFloat();
+        System.out.print("muz kaç kilo ? : ");
+        muz = input.nextFloat();
+        System.out.print("patlıcan kaç kilo ? : ");
+        patlican = input.nextFloat();
+        toplam = (armut * 2.17f) + (elma * 3.67f) + (domates * 1.11f) + (muz * 0.95f) + (patlican * 5.00f);
+        System.out.println("Toplam tutar : "+ toplam);
     }
 }
